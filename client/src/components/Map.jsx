96ae63210c4b48401afb2011300f2f5632f6d9c2
Alex Miller <alexannmill@gmail.com>
2022-11-24 14:43:31 -0800

    map renders local temp data,install of redux

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,24 +1,26 @@
 // --- (1), (2) & (3): install and import ---
 import { MapContainer, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import icon from "leaflet/dist/images/marker-icon.png";
-import axios from "axios"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Markers from "./Markers";
+import axios from "axios"
 
-export function Map() {
-  const [shelters, setShelters] = useState(null)
+
+export default function Map() {
+
   const position = [49.2827, -123.1207];
 
-  //  axios.get(`https://opendata.vancouver.ca/api/records/1.0/search/?dataset=homeless-shelter-locations&q=&lang=en&rows=80&facet=facility&facet=category&facet=meals&facet=pets&facet=carts&facet=geo_local_area`)
-  //  .then((res => {
-  //   const shelters = res.data.records;
-  //   console.log('shelters:', shelters);
-  //   setShelters(shelters);
-  //  }))
+  const [resources, setResources] = useState(null)
 
-
+  useEffect(() => {
+    axios
+    .get(`https://opendata.vancouver.ca/api/records/1.0/search/?dataset=homeless-shelter-locations&q=&lang=en&rows=80&facet=facility&facet=category&facet=meals&facet=pets&facet=carts&facet=geo_local_area`)
+      .then((res => {
+        const incomingData = res.data.records;
+        console.log('incomingData:', incomingData);
+        setResources(incomingData);
+      }))
+  },[])
 
 
   return (
@@ -33,7 +35,7 @@ export function Map() {
             href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Markers key={position} shelters={shelters} position={position}/>
+          <Markers key={position} position={position} resources={resources}/>
         </MapContainer>
     </section>
   );
--- client/src/components/Markers.jsx
+++ client/src/components/Markers.jsx
@@ -2,35 +2,57 @@ import { Marker, Popup} from "react-leaflet";
 import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import icon from "leaflet/dist/images/marker-icon.png";
-import { useState } from "react";
-
-function Markers(props) {
-
-  //props location
-  //set location
+import { useEffect, useState } from "react"
+import axios from "axios"
+import records from "../assets/tempData.json"
 
 
+export default function Markers(props) {
 
-  // const renderMarkers = props.resources.map((resource) => {
-  //   return
-  //   <Marker 
-  //   key={resource}
+  // const [resources, setResources] = useState(null)
+  // //props location
+  // //set location
 
-  //   />
-  // })
+  // useEffect(() => {
+  //   axios
+  //   .get(`https://opendata.vancouver.ca/api/records/1.0/search/?dataset=homeless-shelter-locations&q=&lang=en&rows=80&facet=facility&facet=category&facet=meals&facet=pets&facet=carts&facet=geo_local_area`)
+  //     .then((res => {
+  //       const incomingData = res.data.records;
+  //       console.log('incomingData:', incomingData);
+  //       setResources(incomingData);
+  //     }))
+  // },[])
+console.log('recordes:', records)
+const customIcon = new Icon({
+  iconUrl: icon,
+  iconSize: [20, 30],
+  // iconAnchor: [1, 1],
+  // popupAnchor: [-0, -76]
+});
 
-  const customIcon = new Icon({
-    iconUrl: icon,
-    iconSize: [20, 30],
-    // iconAnchor: [1, 1],
-    // popupAnchor: [-0, -76]
-  });
+    const renderMarkers = (records.records).map((resource) => {
+    return (
+    <Marker 
+    key={resource.recordid}
+    position={[
+      resource.fields.geom.coordinates[1],
+      resource.fields.geom.coordinates[0]
+    ]} 
+    icon={customIcon}
 
-  return props.position === null ? null : (
-    <Marker position={props.position} icon={customIcon}>
-      <Popup>You are here</Popup>
-    </Marker>
-  );
+    />
+    )
+  })
+
+
+  // return props.position === null ? null : (
+  //   {renderMarkers}
+  // );
+
+  return(
+    <div>
+      {renderMarkers}
+    </div>
+  )
 }
 
-export default Markers;
