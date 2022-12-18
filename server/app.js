96ba436793e9e83184bcdfda6cb09c1ed88d4495
Alex Miller <alexannmill@gmail.com>
2022-12-17 17:55:49 -0800

    routing for foodbank, safeI, shelters

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 5001;
+const logger = require("morgan");
 
-const sheltersRouter = require("./routes/shelter");
-const foodbanksRouter = require("./routes/foodbank");
+// ---- Routes
+const sheltersRouter = require("./routes/shelters");
+const foodbanksRouter = require("./routes/foodbanks");
+const safeinjectionsRouter = require("./routes/safeinjections");
+const morgan = require("morgan");
 
+// ---- Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+// ---- Redirection
 app.use("/shelters", sheltersRouter);
 app.use("/foodbanks", foodbanksRouter);
+app.use("/safeinjections", safeinjectionsRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- server/db/queries/shelters.js
+++ server/db/queries/shelters.js
@@ -1,1 +1,9 @@
 const db = require("../db_index");
+
+export const allShelters = () => {
+  return db.query(`SELECT * FROM shelters`).then((res) => {
+    const result = res.rows;
+    console.log("result:", result);
+    return result;
+  });
+};
